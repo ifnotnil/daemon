af1136bf67b71edadbe05e32c7a44c2233b42492
Kostas Stamatakis <moukoublen@gmail.com>
2026-03-17 22:21:45 +0200

    bump go and tools

--- tools/tools.go
+++ tools/tools.go
@@ -4,11 +4,9 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/v2/cmd/golangci-lint"
-	_ "github.com/vektra/mockery/v3"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "honnef.co/go/tools/cmd/staticcheck"
-	_ "mvdan.cc/gofumpt"
+	_ "github.com/itchyny/gojq/cmd/gojq"
 	_ "mvdan.cc/sh/v3/cmd/shfmt"
 )
 
-func main() {}
+func main() {
+}
